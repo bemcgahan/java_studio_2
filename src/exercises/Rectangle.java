809898ce2aea3153baecc88fb856062837891739
Brenna McGahan <brenna.degan@gmail.com>
2020-04-20 19:38:48 -0500

    add files

---
+++ src/exercises/Rectangle.java
@@ -0,0 +1,20 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Rectangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is the length of the rectangle?");
+        double length = input.nextDouble();
+
+        System.out.println("What is the width of the rectangle?");
+        double width = input.nextDouble();
+
+        input.close();
+        double area = length * width;
+        System.out.println("The area is: " + area);
+    }
+
+
+}
